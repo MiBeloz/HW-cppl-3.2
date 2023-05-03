b831172a6eba078bc4d738f37969d4b0ecae6e3a
MiBeloz <miha_bmv_@mail.ru>
2023-05-03 16:21:12 +0600

    Initial commit

---
+++ Smart_array.h
@@ -0,0 +1,147 @@
+﻿#pragma once
+
+#include <iostream>
+#include <exception>
+
+
+template<typename T>
+class Smart_array {
+public:
+	Smart_array(const size_t size);
+
+	void add_element(const T element);
+	T get_element(const size_t element) const;
+	size_t get_size() const;
+	size_t get_capacity() const;
+	const Smart_array<T>* get_address() const;
+
+	//Конструктор копирования
+	Smart_array<T> (const Smart_array<T>& other);
+
+	//Конструктор присваивания
+	Smart_array<T>& operator=(const Smart_array<T>& other);
+
+	~Smart_array();
+
+private:
+	size_t m_capacity;
+	size_t m_size;
+	T* m_array;
+};
+
+
+template<typename T>
+Smart_array<T>::Smart_array(const size_t size) {
+	if (size > SHRT_MAX) {
+		throw std::exception("Smart_array is too long");
+	}
+
+	m_capacity = size;
+	m_size = 0;
+	if (m_capacity != 0) {
+		m_array = new T[m_capacity]{};
+	}
+	else {
+		m_array = nullptr;
+	}
+	
+
+	//В конструкторе нельзя писать в поток вывода, это для примера работы программы.
+	std::cout << "\t///Вызов конструктора для объекта: '" << this << "'///" << std::endl;
+}
+
+template<typename T>
+void Smart_array<T>::add_element(const T element) {
+	if (m_capacity > m_size) {
+		m_array[m_size++] = element;
+	}
+	else {
+		m_capacity = m_capacity * 2;
+
+		T* temp_array = new T[++m_capacity]{};
+		for (size_t i = 0; i < m_size; ++i) {
+			temp_array[i] = m_array[i];
+		}
+
+		temp_array[m_size++] = element;
+
+		delete[] m_array;
+		m_array = temp_array;
+	}
+}
+
+template<typename T>
+T Smart_array<T>::get_element(const size_t element) const {
+	if (element >= m_size) {
+		throw std::exception("Smart_array out of range");
+	}
+	return m_array[element];
+}
+
+template<typename T>
+size_t Smart_array<T>::get_size() const {
+	return m_size;
+}
+
+template<typename T>
+size_t Smart_array<T>::get_capacity() const {
+	return m_capacity;
+}
+
+template<typename T>
+const Smart_array<T>* Smart_array<T>::get_address() const {
+	return this;
+}
+
+//Конструктор копирования
+template<typename T>
+Smart_array<T>::Smart_array(const Smart_array<T>& other) {
+	m_capacity = other.m_capacity;
+	m_size = other.m_size;
+	if (m_capacity != 0) {
+		m_array = new T[m_capacity]{};
+		for (size_t i = 0; i < m_size; ++i) {
+			m_array[i] = other.m_array[i];
+		}
+	}
+	else {
+		m_array = nullptr;
+	}
+}
+
+//Конструктор присваивания
+template<typename T>
+Smart_array<T>& Smart_array<T>::operator=(const Smart_array<T>& other) {
+	if (m_array != nullptr) {
+		delete[] m_array;
+		m_array = nullptr;
+	}
+
+	m_capacity = other.m_capacity;
+	m_size = other.m_size;
+	if (m_capacity != 0) {
+		m_array = new T[m_capacity]{};
+		if (m_size > m_capacity) {
+			throw std::exception("Smart_array error");
+		}
+		for (size_t i = 0; i < m_size; ++i) {
+			m_array[i] = other.m_array[i];
+		}
+	}
+	else {
+		m_array = nullptr;
+	}
+
+	return *this;
+}
+
+template<typename T>
+Smart_array<T>::~Smart_array() {
+	if (m_array != nullptr) {
+		delete[] m_array;
+	}
+	m_array = nullptr;
+
+	//В деструкторе нельзя писать в поток вывода, это для примера работы программы.
+	std::cout << "\t///Вызов деструктора для объекта: '" << this << "'///" << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,43 @@
+﻿#include "Smart_array.h"
+
+#include <iostream>
+
+
+void array_info(const Smart_array<int>& arr);
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	std::cout << "\tКопирование умных массивов.\n" << std::endl;
+
+	try {
+		Smart_array<int> arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+		array_info(arr);
+
+		Smart_array<int> new_array(2);
+		new_array.add_element(44);
+		new_array.add_element(34);
+		array_info(new_array);
+
+		std::cout << "Копирование объекта '" << new_array.get_address() << "' в объект '" << arr.get_address() << "'." << std::endl;
+		arr = new_array;
+		array_info(arr);
+	}
+	catch (const std::exception& ex) {
+		std::cerr << ex.what() << std::endl;
+	}
+
+	system("pause > nul");
+	return 0;
+}
+
+void array_info(const Smart_array<int>& arr) {
+	std::cout << "Smart_array '" << arr.get_address() << "': ";
+	for (size_t i = 0; i < arr.get_size(); ++i) {
+		std::cout << arr.get_element(i) << " ";
+	}
+	std::cout << "\nРазмер массива: " << arr.get_size() << std::endl;
+	std::cout << "Емкость массива: " << arr.get_capacity() << std::endl << std::endl;
+}
